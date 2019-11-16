1465cd46a28d8251704b74f831441c36f1ae6316
nortonion <nkishi@usc.edu>
2019-11-16 12:27:41 -0800

    test

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  /**
+   * TODO,
+   * Call local image
+   * GCP 
+   */
+
+   // Send the output
   res.send({ msg: "Test" });
 });
 
